ce6b0d01ce284fc2a6e6c000e361a6afaad9cc91
zitterbewegung <zitterbewegung@gmail.com>
2011-05-30 02:58:44 -0500

    first commit

---
+++ info.rkt
@@ -0,0 +1,10 @@
+#lang setup/infotab
+(define name "uuid-v4")
+(define blurb '((p "uuid-v4 lets you make v4 uuid's quickly.")))
+(define release-notes '((p "This is a major port that is now compatible with PLT 5.0. It also includes new documentation ")))
+(define primary-file "uuid-v4.ss")
+(define required-core-version "4.1")
+(define repositories '("4.x"))
+(define categories '(devtools))
+(define version "0.4")
+;(define scribblings '(("manual.scrbl" (multi-page))))
---
+++ manual.scribl.rkt
@@ -0,0 +1,27 @@
+#lang scribble/manual
+@title{Welcome to my documentation for UUID v4}
+
+@section{Functions URN, UUID}
+@subsection{URN Example} 
+This is a thunk, no input is required. Also the the values that the real returns is actually randomized and this is only an example of what it will return.
+@racketblock[(urn)
+"urn:uuid:15959F2E-4933-4499-B02F-29BD966A5CBD"
+                   ]
+@subsection{Make-uuid Example}
+Note this is a thunk, no input is required. Also the the values that the real returns is actually randomized and this is only an example of what it will return
+@racketblock[(make-uuid)
+'29DA3AC9-274D-4A65-ACEF-D2B2F62ED2A5] 
+This code was derived from Gambit Scheme and is released under the LGPL
+@section{Licence}
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
